1d4cc70133e1311021415d45a1ee8cc73bcafe6e
root <ben@haresign.net>
2021-02-03 22:31:58 +0000

    day 4 Completed

--- bootcamp/urls.py
+++ bootcamp/urls.py
@@ -2,15 +2,19 @@ from django.urls import path, re_path
 from django.contrib import admin
 
 from products.views import (
-    home_view,
+    #bad_view,
+    search_view,
+    product_create_view,
     product_detail_view,
     product_api_detail_view,
     product_list_view
 )
 
 urlpatterns = [
-    path('search/', home_view),
+    #path('bad-view-dont-use/', bad_view),
+    path('search/', search_view),
     path('products/', product_list_view),
+    path('products/create/', product_create_view),
     path('products/<int:pk>/', product_detail_view),
     #path('products/1/', views.product_api_detail_view),
     re_path(r'api/products/(?P<pk>\d+)/', product_api_detail_view),
